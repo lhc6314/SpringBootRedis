159e5480140bc12888528fa42e3d83415d50744c
lhc6314 <44592320+lhc6314@users.noreply.github.com>
2018-10-30 09:06:24 +0800

    Create RedislearnApplication.java

---
+++ src/main/java/com/lhc/redislearn/RedislearnApplication.java
@@ -0,0 +1,12 @@
+package com.lhc.redislearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedislearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedislearnApplication.class, args);
+    }
+}
